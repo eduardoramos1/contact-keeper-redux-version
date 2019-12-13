8468939e6c2603b8ae5f3fed5cc3eb394ef453ae
Eduardo Ramos <eduardorxs@gmail.com>
2019-12-13 19:02:35 -0300

    criando jwt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const config = require("config");
 
 const User = require("./../models/User");
 
@@ -48,7 +51,23 @@ router.post(
 
 			await user.save();
 
-			res.send("Usuário cadastrado");
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get("secret"),
+				{
+					expiresIn: 360000
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
 			res
