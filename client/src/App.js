d57f24f12cd951cf1c1389848418b7f8332235f8
Eduardo Ramos <eduardorxs@gmail.com>
2019-12-30 18:11:23 -0300

    Inicializado Materialize, criado home page mobile, alterado referencias no index.html e componentes footer e navbar'

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import HomeContent from "./components/Layout/HomeContent";
+import NavBar from "./components/Layout/NavBar";
+import Footer from "./components/Layout/Footer";
+
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -6,9 +13,19 @@ import store from "./store";
 import "./App.css";
 
 function App() {
+	useEffect(() => {
+		M.AutoInit();
+	});
+
 	return (
 		<Provider store={store}>
-			<div className="App"></div>
+			<div className="App">
+				<div className="home-layout">
+					<NavBar />
+					<HomeContent />
+				</div>
+				<Footer />
+			</div>
 		</Provider>
 	);
 }
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="flex-column-end">
+			<footer className="custom-footer deep-purple darken-2">
+				<p className="white-text">Criado por Eduardo Ramos em 2019</p>
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
